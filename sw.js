3bfd34a56681581733ac4fa012e082f998ead9e6
ADITYA SHARMA <sanket.8824@gmail.com>
2026-02-07 12:25:29 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -4,7 +4,6 @@ self.addEventListener("install", (event) => {
       return cache.addAll([
         "/",
         "/index.html",
-        "/login.html",
         "/dashboard.html",
         "/manifest.json"
       ]);
